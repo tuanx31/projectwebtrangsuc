ed13212eb08ae383d876bb696f0b4b6030ebd272
tuanx31 <nguyendinhtuanx31@gmail.com>
2024-01-08 13:08:13 +0700

    ok

--- src/main/java/controller/login.java
+++ src/main/java/controller/login.java
@@ -1,5 +1,6 @@
 package controller;
 
+import dao.Dao;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,56 +8,52 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
-
-import dao.Dao;
-
 
 @WebServlet(urlPatterns = "/loginuser")
 public class login extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispathcher = request.getRequestDispatcher("login.jsp");
-		
-		dispathcher.forward(request, response);
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 	String username = request.getParameter("username");
-		   
-		    String password = request.getParameter("password");
-		   
-		    Dao cDao = new Dao();
-		    
-		  
-		    
-		 if(cDao.login(username,password)==false) {
-		    	request.setAttribute("error", "tài khoản hoặc mật khẩu không chính xác");
-		    	RequestDispatcher dispathcher = request.getRequestDispatcher("login.jsp");
-		    	dispathcher.forward(request, response);
-		    	System.out.println("sai");
-		    }
-		    	else {
-		    		
-		        // Passwords match, proceed with registration
-		    		HttpSession session = request.getSession();
-		    		session.setAttribute("username", username);
-		    		
-		       
-		        request.setAttribute("success", "Đăng Nhập Thành Công");
-		        if(cDao.isAdmin(username)==true) {
-		        	response.sendRedirect("admin/admin.jsp");
-//		        	System.out.println("admin");
-//		        	RequestDispatcher dispathcher = request.getRequestDispatcher("admin.jsp");
-//			    	dispathcher.forward(request, response);
-		        }else {
-		        	response.sendRedirect("index.jsp");
-		        }
-		    }
-	}
+  protected void doGet(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    RequestDispatcher dispathcher = request.getRequestDispatcher("login.jsp");
 
+    dispathcher.forward(request, response);
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(
+    HttpServletRequest request,
+    HttpServletResponse response
+  ) throws ServletException, IOException {
+    String username = request.getParameter("username");
+
+    String password = request.getParameter("password");
+
+    Dao cDao = new Dao();
+
+    if (cDao.login(username, password) == false) {
+      request.setAttribute("error", "tài khoản hoặc mật khẩu không chính xác");
+      RequestDispatcher dispathcher = request.getRequestDispatcher("login.jsp");
+      dispathcher.forward(request, response);
+      System.out.println("sai");
+    } else {
+      // Passwords match, proceed with registration
+      HttpSession session = request.getSession();
+      session.setAttribute("username", username);
+
+      request.setAttribute("success", "Đăng Nhập Thành Công");
+      if (cDao.isAdmin(username) == true) {
+        response.sendRedirect("admin");
+        //		        	System.out.println("admin");
+        //		        	RequestDispatcher dispathcher = request.getRequestDispatcher("admin.jsp");
+        //			    	dispathcher.forward(request, response);
+      } else {
+        response.sendRedirect("index.jsp");
+      }
+    }
+  }
 }
