f24f0c9379da3e4c84630efb707fffae1d5f8862
Lê Đức Anh <leanhthuongx7@gmail.com>
2024-01-09 02:32:55 +0700

    9/1/2:32

--- src/main/java/controller/order.java
+++ src/main/java/controller/order.java
@@ -6,7 +6,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Order;
+import model.account;
+
 import java.io.IOException;
+
+import dao.Dao;
 
 /**
  * Servlet implementation class order
@@ -26,7 +32,29 @@ public class order extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		HttpSession session = request.getSession();
+		Integer idUser = (Integer)  session.getAttribute("id");
+		String total = request.getParameter("total");
+		System.out.println(total);
+		String name = request.getParameter("name");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		
+		String selectedCity = request.getParameter("city");
+        String selectedDistrict = request.getParameter("district");
+        String selectedWard = request.getParameter("ward");
+        String house = request.getParameter("house");
+        
+        int totalAmount = Integer.parseInt(total);
+        
+
+        String address = house+","+selectedWard + "," + selectedDistrict +","+ selectedCity;
+        
+        Order ord = new Order(1,idUser,name,phone,address,email,totalAmount,"");
+        Dao odao = new Dao();
+        odao.InsertOrderdathang(ord);
+		
 	}
+	
 
 }
